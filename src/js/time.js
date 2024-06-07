a1dd1ce4e25442983dccc254515718c4b51c27f9
aislusha96 <arslanova1996ais@list.ru>
2024-06-07 18:57:17 +0300

    коммит

--- src/js/time.js
+++ src/js/time.js
@@ -1,12 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
   // конечная дата, например 1 июля 2024
   const deadline = new Date(2024, 6, 1);
+  // const deadline = new Date();
+  // const currentMinute = currentDate.getMinutes();
+  // const currentSecond = currentDate.getSeconds();
+  // const value = currentMinute % 15;
+  // console.log(value);
   // id таймера
   let timerId = null;
-  // склонение числительных
-  function declensionNum(num, words) {
-    return words[(num % 100 > 4 && num % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(num % 10 < 5) ? num % 10 : 5]];
-  }
+
   // вычисляем разницу дат и устанавливаем оставшееся времени в качестве содержимого элементов
   function countdownTimer() {
     const diff = deadline - new Date();
@@ -19,15 +21,22 @@ document.addEventListener('DOMContentLoaded', function() {
     const t_minutes = Math.floor(diff / 1000 / 60) % 60;
     const t_seconds = Math.floor(diff / 1000) % 60;
 
-    minutes.textContent = t_minutes < 10 ? '0' + t_minutes : t_minutes;
-    seconds.textContent = t_seconds < 10 ? '0' + t_seconds : t_seconds;
+    minutesOne.textContent = Math.trunc(t_minutes/10);
+    minutesTwo.textContent = t_minutes%10;
+    secondsOne.textContent = Math.trunc(t_seconds/10);
+    secondsTwo.textContent = t_seconds%10;
   }
+
   // получаем элементы, содержащие компоненты даты
   const minutes = document.getElementById('minutes');
+  const minutesOne = minutes.querySelector('span:nth-child(1)');
+  const minutesTwo =  minutes.querySelector('span:nth-child(2)');
   const seconds = document.getElementById('seconds');
-
+  const secondsOne = seconds.querySelector('span:nth-child(1)');
+  const secondsTwo = seconds.querySelector('span:nth-child(2)');
   // вызываем функцию countdownTimer
   countdownTimer();
   // вызываем функцию countdownTimer каждую секунду
   timerId = setInterval(countdownTimer, 1000);
 });
+
