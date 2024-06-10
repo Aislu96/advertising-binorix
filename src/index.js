3489acc4ff796429b7d2198f6366cca2891a36d4
aislusha96 <arslanova1996ais@list.ru>
2024-06-10 18:00:12 +0300

    коммит

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./js/common.js";
 import "./js/time.js";
 import "./js/video.js";
+import "./js/app.js";
+import "./js/telephone.js";
 import "./assets/css/main.css";
 import "./assets/scss/main.scss";
 
