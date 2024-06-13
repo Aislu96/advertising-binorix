9bc9fbfebd9f7f54486c4177dc75096a84e84cb6
aislusha96 <arslanova1996ais@list.ru>
2024-06-13 16:59:47 +0300

    коммит

--- src/js/common.js
+++ src/js/common.js
@@ -1,7 +1,11 @@
 const arrowsQuestions =  document.querySelectorAll('.questions__group');
 const blockQuestions = document.querySelectorAll('.questions__body');
 const imgArrow = document.querySelectorAll('.questions__arrow-img');
-
+let scrl= false;
+//Если было совершено событие touchmove, меняем значение переменной на true
+document.addEventListener('touchmove', function(){
+  scrl = true;
+});
 for (let i = 0; i < arrowsQuestions.length; i++) {
   arrowsQuestions[i].addEventListener('click',function () {
     if(!arrowsQuestions[i]) return;
@@ -10,7 +14,11 @@ for (let i = 0; i < arrowsQuestions.length; i++) {
 
   arrowsQuestions[i].addEventListener('touchend', function (event) {
     event.preventDefault();
-    toggleText(i);
+    if (scrl !== true){
+      toggleText(i);
+    }else{
+      scrl = false;
+    }
   });
 }
 
@@ -23,4 +31,12 @@ function toggleText(index) {
 
 
 
+// Устанавливаем таймер на автовоспроизведение через 10 секунд
+setTimeout(function() {
+  var iframe = document.querySelectorAll('.video__play');
+  iframe.forEach((item)=> {
+    let url = item.getAttribute('src') + '?autoplay=1&mute=0&loop=1&playlist=RPCZBBUe474';
+    item.setAttribute('src', url);
+  });
+}, 3000);
 
