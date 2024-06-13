7c3427929d9f3b82ba7c34f1d9de9b5fa77a1a8e
aislusha96 <arslanova1996ais@list.ru>
2024-06-13 11:21:28 +0300

    коммит

--- src/js/common.js
+++ src/js/common.js
@@ -1,6 +1,6 @@
 const arrowsQuestions =  document.querySelectorAll('.questions__group');
 const blockQuestions = document.querySelectorAll('.questions__body');
-const imgArrow = document.querySelectorAll('.questions__arrow span');
+const imgArrow = document.querySelectorAll('.questions__arrow-img');
 
 for (let i = 0; i < arrowsQuestions.length; i++) {
   arrowsQuestions[i].addEventListener('click',function () {
@@ -9,7 +9,6 @@ for (let i = 0; i < arrowsQuestions.length; i++) {
   });
 
   arrowsQuestions[i].addEventListener('touchend', function (event) {
-    // Предотвращаем действие по умолчанию для касания (например, прокрутку страницы)
     event.preventDefault();
     toggleText(i);
   });
@@ -18,7 +17,7 @@ for (let i = 0; i < arrowsQuestions.length; i++) {
 function toggleText(index) {
   if (!arrowsQuestions[index]) return;
   blockQuestions[index].classList.toggle("active");
-  imgArrow[index].style.backgroundImage = 'url("/assets/img/questions/arrowDown.svg")';
+  imgArrow[index].classList.toggle("arrow-active");
 }
 
 
